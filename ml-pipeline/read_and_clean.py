49e664e38c67db901e789f50ad097777c4e0c856
vahuja92 <vedika.ahuja@gmail.com>
2019-04-18 10:47:30 -0500

    almost there

--- ml-pipeline/read_and_clean.py
+++ ml-pipeline/read_and_clean.py
@@ -1,15 +1,18 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 
 '''
 4/18
 Tomorrow:
-	2. Do the explore data section
-	3. Do write-up
+	1. Make histograms
+	2. Do write-up
+	3. Figure out whether to make a confusion matrix
 '''
 
-def read_dataset(filename, data_types=None, parameters=None):
+def read_dataset(filename, data_types=None):
 	'''
 	Takes a csv filepath/filename and saves as a pandas dataframe, with the
 	first row used as the header.
@@ -37,18 +40,54 @@ distributions of variables, correlations between them, find outliers,
 and data summaries.
 '''
 
-def var_distributions(df):
+def describe_data(df):
+	'''
+	Print descriptive statistics of the dataset
+	'''
+	print("Columns in the data")
+	print(df.columns)
+	print("-------------------------------------")
+	print("Summary of data")
+	print("-------------------------------------")
 	print(df.describe())
+	print("-------------------------------------")
+	print ('The data has {} rows and {} columns'.format(df.shape[0],df.shape[1]))
+	print("--------------------------------------")
+	print("The data has the following missing values for each variable")
+	print("--------------------------------------")
+	print(df.isnull().sum().sort_values(ascending=False))
 
-def explore(df):
-	df.describe()
+def distributions(df, target_var):
+	'''
+	Print and export graphs and tables about the distributions of variables
+	in the df.
 
-def cr_box_plot(df):
-	df.boxplot()
-	plt.show()
+	Specify the target variable to find target specific correlations
+	'''
 
-	df.plot()
-	plt.show()
+
+def correlations(df, target_var):
+	'''
+	Print and export correlations between the variables.
+
+	Specify the target variable to find target specific correlations
+	'''
+	# seperate out the Categorical and Numerical features
+	numerical_feature = df.dtypes[(df.dtypes == 'float') | (df.dtypes == 'int')].index
+	categorical_feature=df.dtypes[df.dtypes== 'object'].index
+
+	corr= df[numerical_feature].corr()
+	sns.heatmap(corr, cmap="YlGnBu")
+	plt.savefig('data-exploration/correlation_heat_mpa.png', bbox_inches='tight')
+	plt.close()
+
+	print("5 most postivively correlated variables with target variable")
+	print("")
+	print (corr[target_var].sort_values(ascending=False)[:5], '\n') #top 5 values
+	print("-------------------------------------------")
+	print("5 most negatively correlated variables with target variable")
+	print("")
+	print (corr[target_var].sort_values(ascending=True)[:5], '\n') #top 5 values
 
 '''
 Pre-ProcessData:Forthisassignment,youcanlimitthistofilling
